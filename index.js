e5331263b650bfa252400cdc95e5bc4142bb341a
Anthony McCormick <=>
2013-08-06 22:14:55 +0100

    Register Modularization

--- index.js
+++ index.js
@@ -1,68 +1,12 @@
 'use strict';
 
-//var express = require('express');
-//var app = express();
-var _ = require('lodash');
-
-//var passport = require('passport');
-var log = require('nodelogger')('NodeAuth');
-var Auth;
-
-module.exports.initialize = function (mongoose, connection) {
-    module.exports.NodeAuth = Auth = require('./lib/NodeAuth')(mongoose, connection);
-//    app.configure(function () {
-//        app.use(passport.initialize());
-//        app.use(passport.session());
-//    });
-
-//    app.post('/register', module.exports.register);
-
-    return function (req, res, next) {
-        next();
-    };
-};
-
-module.exports.register = function (req, res, next) {
-    if (invalidEmail(req.body.email)) {
-        next({message: 'api.error.invalid.params', data: 'invalid.email'});
-    } else if (invalidPassword(req.body.pass)) {
-        next({message: 'api.error.invalid.params', data: 'invalid.password'});
-    } else {
-        registerEmail(req, res, next);
-    }
-};
-
-//-------------------------------------------------------------------------
-//
-// Private Methods
-//
-//-------------------------------------------------------------------------
-
-function registerEmail(req, res, next) {
-    Auth.create({email: req.body.email, password: req.body.pass}, function (err, result) {
-        if (!handleError(err, next)) {
-            next(null, result);
-        }
-    });
-}
-
-function handleError(err, next) {
-    if (err) {
-        if (err.name === 'MongoError' && err.code === 11000) {
-            next({message: 'api.error.conflict', data: 'duplicate.email'});
-        } else {
-            log.error('Registering email', err);
-            next({message: 'api.error.server'});
-        }
-        return true;
-    }
-    return false;
-}
-
-function invalidEmail(email) {
-    return _.isArray(email);
-}
-
-function invalidPassword(pass) {
-    return _.isArray(pass);
-}
+/**
+ * Add the registration plugin to your user model before instantiating it.
+ *
+ * schema.plugin(NodeAuth.registrationPlugin);
+ *
+ * You will then be able to call. YourModel.register(email, password, next);
+ * 
+ * @type {*}
+ */
+module.exports.registrationPlugin = require('./lib/NodeAuthRegister');
