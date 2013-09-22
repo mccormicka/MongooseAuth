6d5c4b988e10a49a18deed8e22d914a8eadb6ae3
Anthony McCormick <=>
2013-09-22 02:24:06 +0100

    fixed broken unit tests after user refactor

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -8,7 +8,7 @@ describe('NodeAuthLocal Tests', function () {
     var db = mongoose.createConnection('mongodb://localhost:3001/Whatever');
     var Auth = require('../index');
     var schema = new mongoose.Schema();
-    schema.plugin(Auth.plugin, {local:true, passport:require('passport')});
+    schema.plugin(Auth.plugin, {local: true, passport: require('passport')});
     var User = db.model('localmockuser', schema);
 
     var model;
@@ -27,9 +27,14 @@ describe('NodeAuthLocal Tests', function () {
 
     describe('SHOULD', function () {
         it('Authenticate a valid user', function (done) {
-            User.authenticateLocal({body:{email:'test@test.com', pass:'password'}}, {}, function(err, result){
-                console.log('Error is ', err, result);
-                done(err);
+            User.authenticateLocal({body: {email: 'test@test.com', password: 'password'}}, {}, function (err, result) {
+                expect(result).toBeTruthy();
+                if (result) {
+                    expect(result.email).toBe('test@test.com');
+                    done(err);
+                } else {
+                    done('Error logging in user');
+                }
             });
         });
     });
@@ -37,44 +42,56 @@ describe('NodeAuthLocal Tests', function () {
     describe('SHOULD NOT', function () {
 
         it('Authenticate an invalid email', function (done) {
-            User.authenticateLocal({body:{email:'testfake@test.com', pass:'password'}}, {}, function(err){
+            User.authenticateLocal({body: {email: 'testfake@test.com', password: 'password'}}, {}, function (err) {
                 expect(err).not.toBeNull();
-                if(err){
+                if (err) {
                     expect(err.message).toBe('api.error.invalid.params');
-                    expect(err.data).toEqual([ { field : 'email', reason : 'incorrect' }, { field : 'password', reason : 'incorrect' } ]);
+                    expect(err.data).toEqual([
+                        { field: 'email', reason: 'incorrect' },
+                        { field: 'password', reason: 'incorrect' }
+                    ]);
                 }
                 done();
             });
         });
 
         it('Authenticate without an email', function (done) {
-            User.authenticateLocal({body:{pass:'password'}}, {}, function(err){
+            User.authenticateLocal({body: {password: 'password'}}, {}, function (err) {
                 expect(err).not.toBeNull();
-                if(err){
+                if (err) {
                     expect(err.message).toBe('api.error.invalid.params');
-                    expect(err.data).toEqual([ { field : 'email', reason : 'incorrect' }, { field : 'password', reason : 'incorrect' } ]);
+                    expect(err.data).toEqual([
+                        { field: 'email', reason: 'incorrect' },
+                        { field: 'password', reason: 'incorrect' }
+                    ]);
                 }
                 done();
             });
         });
 
         it('Authenticate an invalid password', function (done) {
-            User.authenticateLocal({body:{email:'test@test.com', pass:'wrongpassword'}}, {}, function(err){
+            User.authenticateLocal({body: {email: 'test@test.com', password: 'wrongpassword'}}, {}, function (err) {
                 expect(err).not.toBeNull();
-                if(err){
+                if (err) {
                     expect(err.message).toBe('api.error.invalid.params');
-                    expect(err.data).toEqual([ { field : 'email', reason : 'incorrect' }, { field : 'password', reason : 'incorrect' } ]);
+                    expect(err.data).toEqual([
+                        { field: 'email', reason: 'incorrect' },
+                        { field: 'password', reason: 'incorrect' }
+                    ]);
                 }
                 done();
             });
         });
 
         it('Authenticate without a password', function (done) {
-            User.authenticateLocal({body:{email:'test@test.com'}}, {}, function(err){
+            User.authenticateLocal({body: {email: 'test@test.com'}}, {}, function (err) {
                 expect(err).not.toBeNull();
-                if(err){
+                if (err) {
                     expect(err.message).toBe('api.error.invalid.params');
-                    expect(err.data).toEqual([ { field : 'email', reason : 'incorrect' }, { field : 'password', reason : 'incorrect' } ]);
+                    expect(err.data).toEqual([
+                        { field: 'email', reason: 'incorrect' },
+                        { field: 'password', reason: 'incorrect' }
+                    ]);
                 }
                 done();
             });
