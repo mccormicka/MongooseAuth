410f15a8d127086c94a669d334261c342fa19903
Anthony McCormick <=>
2013-10-10 23:58:03 +0100

    lowercased requires() so that they do not conflict with build servers

--- test/password/PasswordChange.spec.js
+++ test/password/PasswordChange.spec.js
@@ -2,7 +2,7 @@
 
 describe('NodeAuthPassword Tests', function () {
 
-    var mockgoose = require('Mockgoose');
+    var mockgoose = require('mockgoose');
     var mongoose = require('mongoose');
     mockgoose(mongoose);
     var db = mongoose.createConnection('mongodb://localhost:3001/Whatever');
